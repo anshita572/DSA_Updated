47870de58c25b06cf64a5b2157a1449b9f23b1cc
Anshita <anshita8150@gmail.com>
2022-07-08 18:44:53 +0530

    commit

--- Web Development/Dice_game/index.js
+++ Web Development/Dice_game/index.js
@@ -7,13 +7,13 @@ var randomImageSource2="./images/dice"+randomNumber2+".png";
 document.querySelector("img.img2").setAttribute("src",randomImageSource2);
 var winner;
 if(randomNumber1 > randomNumber2)
-{ document.querySelector("h1.container").innerHTML="Player 1 Wins !🚩";
+{ document.querySelector("h1").innerHTML="Player 1 Wins !🚩";
 
 }
 else if(randomNumber2 > randomNumber1)
-{ document.querySelector("h1.container").innerHTML="Player 2 Wins !🚩";
+{ document.querySelector("h1").innerHTML="Player 2 Wins !🚩";
 
 }
 else{
-    document.querySelector("h1.container").innerHTML="Draw !";
+    document.querySelector("h1").innerHTML="Its'a Draw !";
 }
