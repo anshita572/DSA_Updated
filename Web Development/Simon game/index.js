b6303d85611b5c5563160177b03e1c4d03d70376
Anshita <anshita8150@gmail.com>
2022-07-12 17:11:00 +0530

    update simon

--- Web Development/Simon game/index.js
+++ Web Development/Simon game/index.js
@@ -1,8 +1,28 @@
+var buttonColors=["red","blue","green","yellow"];
+var gamePattern=[];
+var userClickedPattern=[];
+$(".btn").on("click",function()
+{
+    var userChosenColor=$(this).attr("id");
+    userClickedPattern.push(userChosenColor);
+    // console.log(userClickedPattern);
+    playButtonSound(userChosenColor);
+    
+});
+function playButtonSound(name)
+{
+    var playSound=new Audio("./sounds/"+name+".mp3");
+    playSound.play();
+}
+
+
+//randomly a color is chosen,it animates and plays its sound automatically randomly for the next level
 function nextSequence(){
 var randomNumber=Math.floor((Math.random())*4);
-}
-var buttonColors=["red","blue","green","yellow"];
+
 var randomChosenColor=buttonColors[randomNumber];
-var gamePattern=[];
 gamePattern.push(randomChosenColor);
-$("#"+randomChosenColor);
+$("#"+randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
+playButtonSound(randomChosenColor);
+
+}
