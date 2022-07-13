e253758566336e886c0562a25f9c31bd618d56cc
Anshita <anshita8150@gmail.com>
2022-07-14 00:42:19 +0530

    update simon

--- Web Development/Simon game/index.js
+++ Web Development/Simon game/index.js
@@ -3,15 +3,24 @@ var gamePattern = [];
 var userClickedPattern = [];
 var started = false;
 var level=0;
-var green=false;
-$(document).keydown(function(e)
-{
-    if(e.key==='g')
-    {
-        var greenSound = new Audio("./sounds/green.mp3");
-     greenSound.play();
-     green=true;
-    clickOrKeypress();
+var highscore=0;
+var score=0;
+// var green=false;
+// $(document).keydown(function(e)
+// {
+//     if(e.key==='g')
+//     {$(".btn").trigger("click", function () {
+//       var userChosenColor = $(this).attr("id");
+//       userClickedPattern.push(userChosenColor);
+//       // console.log(userClickedPattern);
+//       playButtonSound(userChosenColor);
+//       animatePress(userChosenColor);
+//       checkAnswer(userClickedPattern.length-1);
+//     });
+//         var greenSound = new Audio("./sounds/green.mp3");
+//      greenSound.play();
+//      green=true;
+   // clickOrKeypress();
   //  $(".g").addClass("pressed");
   // userClickedPattern.push(e.key);
   // var userChosenColor = $(e.key).attr("id");
@@ -21,18 +30,18 @@ $(document).keydown(function(e)
   //animatePress(e.key);
   // animatePress(userChosenColor);
     
-    
-}
-});
-function clickOrKeypress()
-{ if(green===true)
-  {var userChosenColor = $("e.key").attr("id");
-userClickedPattern.push(userChosenColor);
-// console.log(userClickedPattern);
-playButtonSound(userChosenColor);
-animatePress(userChosenColor);
-checkAnswer(userClickedPattern.length-1);}
-}
+//   }   
+// }
+// );
+// function clickOrKeypress()
+// { 
+//   var userChosenColor = $("e.key"+id);
+// userClickedPattern.push(userChosenColor);
+// // console.log(userClickedPattern);
+// playButtonSound(userChosenColor);
+// animatePress(userChosenColor);
+// checkAnswer(userClickedPattern.length-1);}
+
 $(".btn").on("click", function () {
   var userChosenColor = $(this).attr("id");
   userClickedPattern.push(userChosenColor);
@@ -53,9 +62,10 @@ function animatePress(currentColor) {
 }
 $(document).keypress(function (event) {
   // console.log(event.key);
-  if (started === false) {
+  if (event.keyCode===13 && started === false) {
     // $("h1").html("Level "+level);
     nextSequence();
+    $(".score-class").html("Score : "+score);
     started = true;
    
   }
@@ -63,8 +73,12 @@ $(document).keypress(function (event) {
 function restart()
 {
     level=0;
+    $(".score-class").html("Score : "+score);
+    score=0;
+    
     gamePattern=[];
     started=false;
+   
 }
 function checkAnswer(currentLevel)
 {if(userClickedPattern[currentLevel]===gamePattern[currentLevel])
@@ -74,6 +88,12 @@ if(userClickedPattern.length===gamePattern.length)
 {
     setTimeout(function()
     {nextSequence();},100);
+    score++;
+    $(".score-class").html("Score : "+score);
+    if(level > highscore)
+    {
+      highscore=level;
+    $(".highscore-class").html("Highscore : "+highscore);}
 }}
 else
 {
@@ -83,15 +103,19 @@ else
   $("body").addClass("game-over");
   setTimeout(function()
   {$("body").removeClass("game-over");},200);
-  $("h1").text("Game Over,Press any key to restart");
+  $("h1").text("Game Over,Press Enter key to restart");
   restart();
+  
 }}
 
 //randomly a color is chosen,it animates and plays its sound automatically randomly for the next level
 function nextSequence() {
     userClickedPattern=[];
+   
     level++;
+    //highscore++;
   $("h1").html("Level "+level);
+  //$("h2").html("Highscore = "+highscore);
   var randomNumber = Math.floor(Math.random() * 4);
 
   var randomChosenColor = buttonColors[randomNumber];
