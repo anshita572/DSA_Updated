d430f1e6c79ef92394b49596ffe6105922eaabb8
Anshita <anshita8150@gmail.com>
2022-07-22 00:55:43 +0530

    update snake game

--- Web Development/Snake game/index.js
+++ Web Development/Snake game/index.js
@@ -1,30 +1,25 @@
+const musicSound = new Audio("./music/music.mp3");
 let direction = { x: 0, y: 0 }; //jis direction m we r moving the snake
 var score = 0;
+var highscore = 0;
 var snakeHead = [{ x: 9, y: 9 }]; //initially snake is in 11th row and 11th column
 var food = { x: 5, y: 5 }; //initially food is in 5th row and 5th column
 var lastRenderTime = 0;
-var snakeSpeed = 7;
+var snakeSpeed = 7; //initially snake speed
 function main(currentTime) {
   //game loop
   window.requestAnimationFrame(main);
   var secondsTillLastRender = (currentTime - lastRenderTime) / 1000; //1000 so as to convert in seconds
   if (secondsTillLastRender < 1 / snakeSpeed) {
-    //snake will move 5 times(=snake speed) in a second
-    // if secondsTillLastRender <1/2 ,don't render
+    //snake will move 7 times(=snake speed) in a second
+    // if secondsTillLastRender <1/7 ,don't render
     return;
   }
   // console.log("render");
   lastRenderTime = currentTime;
- // start();
+ 
   gameOperations();
 }
-// function start()
-// {$(document).keypress(function (e) {
-//   if (e.keyCode==="Enter")
-//   {gameOperations();}
-// }
-// );
-// }
 window.requestAnimationFrame(main);
 function isCollide(snake) {
   //if snake bumps into itself
@@ -34,24 +29,59 @@ function isCollide(snake) {
     }
   }
   //if snake bumps into any of the walls
-  if (
-  snake[0].x >= 18 ||
-    snake[0].x <= 0 || snake[0].y >= 18 ||
-    snake[0].y <= 0
-  ) {
+  if(snake[0].x >= 19 || snake[0].x <=0 || snake[0].y >= 19 || snake[0].y <=0){
+    // console.log(snake[0].x);
+    // console.log(snake[0].y);
     return true;
-  }
+}
+    
   return false;
 }
 
 function gameOperations() {
+  //if snake collides into any wall or itself,restart the game
+  if (isCollide(snakeHead)) {
+    var gameOverSound = new Audio("./music/gameover.mp3");
+    musicSound.pause();
+    gameOverSound.play();
+
+    $(".heading").html("Game over !<br> Press any key to <br> play again");
+    //set snakeHead,direction,food,score,snakeSpeed to initial values
+    snakeHead = [{ x: 9, y: 9 }];
+    direction = { x: 0, y: 0 };
+    food = { x: 5, y: 5 };
+    snakeSpeed = 7;
+    $(".score-class").text("Score : " + score);
+    score = 0;
+  }
+  //to display snake array
+  const gameBoard = document.getElementById("board");
+  gameBoard.innerHTML = "";
+  snakeHead.forEach((snakeSegment, index) => {
+    var snakeElement = document.createElement("div");
+    snakeElement.style.gridRowStart = snakeSegment.y;
+    snakeElement.style.gridColumnStart = snakeSegment.x;
+    if (index === 0) {
+      snakeElement.classList.add("snake-face");
+    } else {
+      snakeElement.classList.add("snake-head");
+    }
+
+    gameBoard.appendChild(snakeElement);
+  });
+  //to display food
+  var foodElement = document.createElement("div");
+  foodElement.style.gridRowStart = food.x;
+  foodElement.style.gridColumnStart = food.y;
+  foodElement.classList.add("snake-food");
+  gameBoard.appendChild(foodElement);
+
+  //playing the game with arrow keys
   document.addEventListener("keydown", function (e) {
-  
-    // var moveSound = new Audio("./music/move.mp3");
-    // moveSound.play();
-    // $("h1").remove();
-    direction = { x: 1, y: 0 };
-    $(".heading").text("Enjoy the amazing adventure !");
+    musicSound.play();
+    direction = { x: 1, y: 0 };//initial direction in which snake starts moving
+    $(".heading").html("Enjoy the amazing <br> adventure !");
+    $(".score-class").text("Score : " + score);
     //  console.log(event.key);
 
     switch (e.key) {
@@ -79,23 +109,7 @@ function gameOperations() {
       default:
         break;
     }
-  
   });
-
-
-  //if snake has eaten food ,regenerate food randomly
-  if (snakeHead[0].x === food.x && snakeHead[0].y === food.y) {
-    //if coordinates of snake Head and food meet=>snake has eaten food
-    var foodSound = new Audio("./music/food.mp3");
-    foodSound.play();
-    snakeHead.unshift({
-      x: snakeHead[0].x + direction.x,
-      y: snakeHead[0].y + direction.y,
-    });
-    score++;
-    var randomFoodPosition = Math.floor(Math.random() * 18) + 1;
-    food = { x: randomFoodPosition, y: randomFoodPosition };
-  }
   //moving the snake
   for (var i = snakeHead.length - 2; i >= 0; i--) {
     snakeHead[i + 1] = { ...snakeHead[i] };
@@ -103,33 +117,31 @@ function gameOperations() {
   snakeHead[0].x = snakeHead[0].x + direction.x;
   snakeHead[0].y = snakeHead[0].y + direction.y;
 
-  //to display snake array
-  const gameBoard = document.getElementById("board");
-  gameBoard.innerHTML = "";
-  snakeHead.forEach((snakeSegment) => {
-    var snakeElement = document.createElement("div");
-    snakeElement.style.gridRowStart = snakeSegment.y;
-    snakeElement.style.gridColumnStart = snakeSegment.x;
-    snakeElement.classList.add("snake-head");
-    gameBoard.appendChild(snakeElement);
-  });
-  //to display food
-  var foodElement = document.createElement("div");
-  foodElement.style.gridRowStart = food.x;
-  foodElement.style.gridColumnStart = food.y;
-  foodElement.classList.add("snake-food");
-  gameBoard.appendChild(foodElement);
-
-  //to update snake array and food
-  if (isCollide(snakeHead)) {
-    var gameOverSound = new Audio("./music/gameover.mp3");
-    gameOverSound.play();
-    
-   $("h1").text("Game over ! Press any key to play again");
-    snakeHead = [
-      { x: 9, y: 9 }
-    ];
-    direction = { x: 0, y: 0 };
-    score = 0;
+  //if snake has eaten food ,regenerate food randomly
+  if (snakeHead[0].x === food.x && snakeHead[0].y === food.y) {
+    //if coordinates of snake Head and food meet=>snake has eaten food
+    var foodSound = new Audio("./music/food.mp3");
+    foodSound.volume = 1; //100%
+    foodSound.play();
+    snakeHead.unshift({
+      x: snakeHead[0].x + direction.x,
+      y: snakeHead[0].y + direction.y,
+    });
+    score++;
+    //update highscore
+    if (score > highscore) {
+      highscore++;
+    }
+    $(".score-class").text("Score : " + score);
+    $(".hiscore-class").text("Highscore : " + highscore);
+    //after increment of 3 points,increase speed by 4
+    for (var i = 0; i < score; i++) {
+      if (score === 3 * i) {
+        snakeSpeed = snakeSpeed + 4;
+      }
+      // console.log(snakeSpeed);
+    }
+    var randomFoodPosition = Math.floor(Math.random() * 18) + 1;
+    food = { x: randomFoodPosition, y: randomFoodPosition };
   }
 }
