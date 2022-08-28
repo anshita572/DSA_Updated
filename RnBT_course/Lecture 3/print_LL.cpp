a94dc2bc552126db27b2aa74cd884faac9205a7d
Anshita <anshita8150@gmail.com>
2022-08-28 18:01:59 +0530

    push Lecture 3

--- RnBT_course/Lecture 3/print_LL.cpp
+++ RnBT_course/Lecture 3/print_LL.cpp
@@ -27,22 +27,20 @@ void insertAtTail(node *&tail, int d)
 }
 void printNode(node *&head)
 {
-    node *temp = head;
-    while (temp != NULL) // loop until temp reaches the last node and points to NULL
-    {
-        cout << temp->data << " "; // printing data of node to which temp is pointing
-        temp = temp->next;         // updating temp(like temp++)
-    }
-    cout << endl;
+   if(head==NULL)
+   {return;}
+   cout<<head->data<<" ";
+   printNode(head->next);
+
 }
 int main()
 {
     node *node1 = new node(10);
     node *head = node1;
     node *tail = node1;
-    printNode(head);
+    // printNode(head);
     insertAtTail(tail, 12);
-    printNode(head);
+    // printNode(head);
     insertAtTail(tail, 15);
     printNode(head);
 
