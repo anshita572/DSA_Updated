f42cff36ee813095fa49a74781887d6554d8a760
Anshita <anshita8150@gmail.com>
2022-09-01 15:57:27 +0530

    commit

---
+++ 450 DSA BABBAR/Recursion_kunal_ques/Basic/reverse_string.cpp
@@ -0,0 +1,17 @@
+//https://leetcode.com/problems/reverse-string/
+#include<iostream>
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    void solve(vector<char>& s,int l,int h)
+    {
+        if(l>h)
+        {return;}
+        swap(s[l],s[h]);
+        solve(s,l+1,h-1);
+    }
+    void reverseString(vector<char>& s) {
+        solve(s,0,s.size()-1);
+    }
+};
