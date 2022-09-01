bd1d41a0fc0629053b04de70ddefeca5663514c0
Anshita <anshita8150@gmail.com>
2022-09-01 23:05:04 +0530

    push

--- 450 DSA BABBAR/Recursion_kunal_ques/Basic/reverse_string.cpp
+++ 450 DSA BABBAR/Recursion_kunal_ques/Basic/reverse_string.cpp
@@ -4,14 +4,21 @@
 using namespace std;
 class Solution {
 public:
-    void solve(vector<char>& s,int l,int h)
+    // void solve(vector<char>& s,int l,int h)
+    // {
+    //     if(l>=h)
+    //     {return;}
+    //     swap(s[l],s[h]);
+    //     solve(s,l+1,h-1);
+    // }
+    // void reverseString(vector<char>& s) {
+    //     solve(s,0,s.size()-1);
+    // }
+    void solve(string s ,string &ans,int i)
     {
-        if(l>h)
+        if(i==s.length())
         {return;}
-        swap(s[l],s[h]);
-        solve(s,l+1,h-1);
-    }
-    void reverseString(vector<char>& s) {
-        solve(s,0,s.size()-1);
+        solve(s,ans,i+1);
+        ans.push_back(s[i]);
     }
 };
