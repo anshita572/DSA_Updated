bb9dc9e0771b8206962f094405a2e91eef6b367b
Anshita <anshita8150@gmail.com>
2022-10-04 01:13:23 +0530

    push is_subseq ques

---
+++ Leetcode Ques/Recursion_ques/is_subseq.cpp
@@ -0,0 +1,27 @@
+//https://leetcode.com/problems/is-subsequence/
+#include<iostream>
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    void solve(string s, string t,int i,int j,int& count)
+    {
+        if(j==t.length())
+        {return;}
+        if(s[i]==t[j])
+        {count++;
+            solve(s,t,i+1,j+1,count);}
+        else //if(s[i]!=t[j])
+        {solve(s,t,i,j+1,count);}
+    }
+    bool isSubsequence(string s, string t) {
+        int i=0;
+        int j=0;
+        int count=0;
+        solve(s,t,i,j,count);
+        if(count==s.length())
+        {return true;}
+        else
+        {return false;}
+    }
+};
