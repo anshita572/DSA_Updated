727fcd701bc421449612d70baeea8ae86c61febb
Anshita <anshita8150@gmail.com>
2022-10-09 22:59:20 +0530

    update print_decreasing.cpp

--- RnBT_course/Lecture 1/print_decreasing.cpp
+++ RnBT_course/Lecture 1/print_decreasing.cpp
@@ -1,9 +1,10 @@
+//TC : O(n)
 #include<iostream>
 #include <bits/stdc++.h>
 using namespace std;
 void print_dec(int n)
 {
-    if(n==0)
+    if(n==0) //base case
     {return;}
     cout<<n<<" ";
     print_dec(n-1);
