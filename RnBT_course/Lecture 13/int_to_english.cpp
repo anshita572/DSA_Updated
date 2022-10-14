95db178f1eb3647d36bf23773f189e308ac5337f
Anshita <anshita8150@gmail.com>
2022-10-14 23:58:55 +0530

    update int_to_english.cpp

--- RnBT_course/Lecture 13/int_to_english.cpp
+++ RnBT_course/Lecture 13/int_to_english.cpp
@@ -11,27 +11,24 @@ public:
     {13, "Thirteen "}, {12, "Twelve "}, {11, "Eleven "}, {10, "Ten "}, {9, "Nine "}, {8, "Eight "}, 
     {7, "Seven "}, {6, "Six "}, {5, "Five "}, {4, "Four "}, {3, "Three "}, {2, "Two "}, {1, "One "}};
     
-    string solve(int n) {
+    string solve(int num)
+    {
         //base case
-        if(n == 0)
-            return "Zero ";
-        
-        for(auto i: arr) {
-            if(n >= i.first) {
-                
-                string first = (n>=100 ? solve(n/i.first) : "") ;
-                string second  = i.second;
-                string remAns = (n%i.first == 0  ? "" : solve(n % i.first)) ;    
-                    
-                return first + second + remAns;     
+        if(num==0)
+        {return "Zero ";}
+        for(auto i:arr)
+        {
+            if(num >= i.first)
+            {string number= (num>=100 ? solve(num/i.first) : ""); 
+            string placeValue=i.second;
+            string remaining=(num%i.first==0 ? "" :solve(num % i.first));
+            return number+placeValue+remaining;
             }
         }
-        
-        return "";   
+        return "";
     }
     string numberToWords(int num) {
-        string ans = solve(num);
-        //to remove last character of answer string, we need to pop back 
+        string ans=solve(num);
         ans.pop_back();
         return ans;
     }
