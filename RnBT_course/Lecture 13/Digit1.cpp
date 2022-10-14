bb5218ecdc85b70d325a10c8183fc9cb42f52606
Anshita <anshita8150@gmail.com>
2022-10-14 20:31:09 +0530

    push Digit1.cpp

---
+++ RnBT_course/Lecture 13/Digit1.cpp
@@ -0,0 +1,36 @@
+//https://leetcode.com/problems/number-of-digit-one/
+#include<iostream>
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+        int getLength(int n)
+    {
+        string s= to_string (n);
+        int len=s.length();
+        //OR
+        // while(n!=0)
+        // {
+        //     n=n/10;
+        //     len++;
+        // }
+        return len;
+        
+    }
+    int countDigitOne(int n) {
+        if(n==0)
+        {return 0;}
+        if(n<10)
+        {return 1;}
+        int len=getLength(n);
+        int base=pow(10,len-1);
+        int remainder=n%base;
+        int firstDigit=n/base;
+        int startingDig1=0;
+        if(firstDigit==1)
+        {startingDig1=n-base+1;}
+        else
+        {startingDig1=base;}
+        return countDigitOne(remainder)+(firstDigit*countDigitOne(base-1))+startingDig1 ;
+    }
+};
