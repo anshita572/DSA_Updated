9aa0c51107b52700b1e2ae78b527e3312e0774bd
Anshita <anshita8150@gmail.com>
2022-10-20 13:18:26 +0530

    update fibonacci.cpp

--- RnBT_course/Recursion/Lecture 1/fibonacci.cpp
+++ RnBT_course/Recursion/Lecture 1/fibonacci.cpp
@@ -1,4 +1,6 @@
+//https://leetcode.com/problems/fibonacci-number/
 // fibonacci series : 0,1,1,2,3,5,8,13...
+//M1 : Recursion
 //TC : O(2^n)
 //SC : O(n)
 #include <iostream>
@@ -24,3 +26,19 @@ int main()
     cout << n << "th term : " << fibonacci(n) << endl;
     return 0;
 }
+//M2 : Iterative (TC : O(n)  SC : O(1))
+class Solution {
+public:
+    int fib(int n) {
+      if(n==0 || n==1)
+      {return n;}
+        int a=0;
+        int b=1;
+        int c;
+        for(int i=1;i<n;i++)
+        {c=a+b;
+        a=b;
+        b=c;}
+        return c;
+    }
+};
