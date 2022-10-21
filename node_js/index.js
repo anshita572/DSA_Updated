736f11dcd7e092bd1596f3eeea9462c11a78a63e
Anshita <anshita8150@gmail.com>
2022-10-21 14:01:33 +0530

    push supervillain package

--- node_js/index.js
+++ node_js/index.js
@@ -1,5 +1,8 @@
 // const fs=require("fs");
 // fs.copyFileSync("file1.txt","file2.txt");
 const superheroes=require("superheroes");
+const supervillains=require("supervillains");
 var mySuperhero=superheroes.random();
 console.log(mySuperhero);
+var myVillain=supervillains.random();
+console.log(myVillain);
