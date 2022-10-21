01405cd41c025d80211172d272e0cff37cc602ed
Anshita <anshita8150@gmail.com>
2022-10-21 13:54:25 +0530

    push package.json

--- node_js/index.js
+++ node_js/index.js
@@ -1,2 +1,5 @@
-const fs=require("fs");
-fs.copyFileSync("file1.txt","file2.txt");
+// const fs=require("fs");
+// fs.copyFileSync("file1.txt","file2.txt");
+const superheroes=require("superheroes");
+var mySuperhero=superheroes.random();
+console.log(mySuperhero);
