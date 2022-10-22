a1fe4a55ef4ba474196e2fda8dd3c8d978a4b885
Anshita <anshita8150@gmail.com>
2022-10-23 00:09:18 +0530

    update calculator

--- Web Development/Backend Development/Calculator/calculator.js
+++ Web Development/Backend Development/Calculator/calculator.js
@@ -1,6 +1,8 @@
 const express=require("express");
 const app=express();
 app.get("/",function(req,res)
-{res.send("hello");})
+{res.sendFile(__dirname + "/index.html");}) 
+//__dirname will give file path of calculator.js
+// console.log(__dirname);
 app.listen(3000,function()
 {console.log("Server is running on port 3000");});
