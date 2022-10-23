51134d84edc908d06381e56d65c1a514afc1a6bb
Anshita <anshita8150@gmail.com>
2022-10-23 12:55:30 +0530

    update calculator.js

--- Web Development/Backend Development/Calculator/calculator.js
+++ Web Development/Backend Development/Calculator/calculator.js
@@ -1,10 +1,15 @@
 const express=require("express");
+const bodyParser=require("body-parser");
 const app=express();
+app.use(bodyParser.urlencoded({extend:true}));
 app.get("/",function(req,res)
 {res.sendFile(__dirname + "/index.html");}) 
 //__dirname will give file path of calculator.js
 // console.log(__dirname);
-app.post("/",function(req,res)
-{res.send("Thanks");})
+app.post("/",function(req,res) //app.post to handle any post requests
+{var num1=Number(req.body.first_num);
+    var num2=Number(req.body.second_num);
+    var ans=num1+num2;
+    res.send("Result of addition = "+ans);})
 app.listen(4000,function()
 {console.log("Server is running on port 4000");});
