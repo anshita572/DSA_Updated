e41898e8775dd39c6c5841236c981808048cc4de
Anshita <anshita8150@gmail.com>
2022-10-24 23:49:20 +0530

    push BMI server.js

--- Web Development/Backend Development/BMI_calculator/index.js
+++ Web Development/Backend Development/BMI_calculator/index.js
@@ -1,6 +1,14 @@
 const express=require("express");
+const bodyParser=require("body-parser");
 const app=express();
+app.use(bodyParser.urlencoded({extend:true}));
+app.get("/",function(req,res)
+{res.sendFile(__dirname+"/index.html");})
+app.post("/",function(req,res)
+{var w=Number(req.body.weight);
+var h=Number(req.body.height);
+var ans=w/h;
+res.send("Your BMI = "+ans);
+})
 app.listen(3000,function()
 {console.log("Server is running on port 3000");})
-app.get("/",function(req,res)
-{res.send("Hello");})
