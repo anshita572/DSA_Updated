4acd0b53f3f82a70f6f6bfbefe8ce9ad952376ea
Anshita <anshita8150@gmail.com>
2022-10-26 18:58:51 +0530

    update

--- POTD/Leetcode POTD/equivalent_strings.cpp
+++ POTD/Leetcode POTD/equivalent_strings.cpp
@@ -26,7 +26,7 @@ class Solution {
 public:
     bool arrayStringsAreEqual(vector<string>& word1, vector<string>& word2) {
         int i=0,j=0;
-        int m=0,n=0;
+        int m=0,n=0;//for that particular letter in an individual string of a word
         while(i<word1.size() && j < word2.size())
         {
             if(word1[i][m]!=word2[j][n])
