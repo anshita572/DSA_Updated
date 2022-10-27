41e1bc016d8684d363453285d6479274399f9bdb
Anshita <anshita8150@gmail.com>
2022-10-27 22:27:38 +0530

    update height.cpp

--- RnBT_course/Recursion/Lecture 5/height.cpp
+++ RnBT_course/Recursion/Lecture 5/height.cpp
@@ -1,4 +1,6 @@
 //https://leetcode.com/problems/maximum-depth-of-binary-tree/
+
+//M1 : count while returning
 class Solution {
 public:
     int maxDepth(TreeNode* root) {
@@ -8,5 +10,18 @@ public:
         int rightAns=maxDepth(root->right);
         int ans=1+max(leftAns,rightAns);
         return ans;
+    }
+};
+//M2 : count while traversing
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if(root==NULL)
+        {return 0;}
+        int leftAns=1+maxDepth(root->left);
+        int rightAns=1+maxDepth(root->right);
+        int ans=max(leftAns,rightAns);
+        return ans;
+        
     }
 };
