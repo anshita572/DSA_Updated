67fe4a2f5b7211bad69f1bf6211ef74826932e68
Anshita <anshita8150@gmail.com>
2022-10-28 13:30:52 +0530

    push subset_XOR_sum.cpp

---
+++ RnBT_course/Backtracking/Lecture 2/subset_XOR_sum.cpp
@@ -0,0 +1,26 @@
+//https://leetcode.com/problems/sum-of-all-subset-xor-totals/
+// TC : O(2^n) SC:O(n)
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    void solve(vector<int>& nums,int i,int &XOR,int &sum)
+    {if(i==nums.size())
+    {sum=sum+XOR;
+    return;}
+     //include
+    XOR=XOR^nums[i];
+     solve(nums,i+1,XOR,sum);
+         XOR=XOR^nums[i]; //backtrack
+     //exclude
+     solve(nums,i+1,XOR,sum);
+     
+    }
+    int subsetXORSum(vector<int>& nums) {
+        int sum=0;
+        int XOR=0;
+        solve(nums,0,XOR,sum);
+        return sum;
+    }
+};
