41348edb1cfb87765ab0d2bf086ad2d4a4f5410b
anshita <anshita8150@gmail.com>
2022-12-23 19:13:22 +0530

    push app.js

---
+++ udemy_node/notes_app/app.js
@@ -0,0 +1,8 @@
+const chalk=require('chalk')
+console.log(chalk.bold('Hey there'))
+// console.log(process.argv[2])
+const command=process.argv[2]
+if(command==='add')
+{console.log("Adding notes ! ")}
+else if(command==='remove')
+{console.log("Removing notes ! ")}
