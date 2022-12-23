5228be21292ac2f3630d718d53ead5779a27d64c
anshita <anshita8150@gmail.com>
2022-12-23 16:42:52 +0530

    push intro1.js

---
+++ udemy_node/notes_app/intro2.js
@@ -0,0 +1,6 @@
+// const add=require('./utils.js')
+// const sum=add(2,3)
+// console.log(sum)
+const getNotes=require('./utils.js')
+const output=getNotes()
+console.log(output)
---
+++ udemy_node/notes_app/utils.js
@@ -0,0 +1,8 @@
+// console.log('This is utlis.js file')
+// const myName='Anshita'
+// const add=function(a,b)
+// {return a+b}
+// module.exports=add
+const getNotes=function()
+{return "Your notes..."}
+module.exports=getNotes
