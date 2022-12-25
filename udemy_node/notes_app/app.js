185e7eadcccf4cc856accdaadfd8b63267745cc1
anshita <anshita8150@gmail.com>
2022-12-25 18:18:54 +0530

    new commit

--- udemy_node/notes_app/app.js
+++ udemy_node/notes_app/app.js
@@ -1,8 +1,10 @@
+const yargs=require('yargs')
 const chalk=require('chalk')
 console.log(chalk.bold('Hey there'))
-// console.log(process.argv[2])
-const command=process.argv[2]
-if(command==='add')
-{console.log("Adding notes ! ")}
-else if(command==='remove')
-{console.log("Removing notes ! ")}
+console.log(process.argv)
+console.log(yargs.argv)
+// const command=process.argv[2]
+// if(command==='add')
+// {console.log("Adding notes ! ")}
+// else if(command==='remove')
+// {console.log("Removing notes ! ")}
