b64cff868daa4352f24f166f2f03ed40f35ce7f3
anshita <anshita8150@gmail.com>
2022-12-26 01:15:29 +0530

    update json_1.js

--- udemy_node/playground/json_1.js
+++ udemy_node/playground/json_1.js
@@ -4,8 +4,18 @@ const book={
     author : "Fiago"
 }
 const bookJson=JSON.stringify(book)
-console.log(bookJson)
+// console.log(bookJson)
 // console.log(bookJson.title) //error
 const bookParse=JSON.parse(bookJson)
-console.log(bookParse.title)
-fs.writeFileSync('json_1.json',bookJson)
+// console.log(bookParse.title)
+// fs.writeFileSync('json_1.json',bookJson)
+const dataBuffer=fs.readFileSync('json_1.json')
+// console.log(dataBuffer)
+const dataJSON=dataBuffer.toString()
+// console.log(dataJSON)
+const data=JSON.parse(dataJSON)
+console.log(data.title)
+data.title="Ego"
+data.author="Jessi"
+const newData=JSON.stringify(data)
+fs.writeFileSync('json_1.json',newData)
