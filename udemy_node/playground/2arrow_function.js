ca4c52a55b02a93d3c4849c407d964d5370a51bf
anshita <anshita8150@gmail.com>
2022-12-27 12:59:29 +0530

    create 2arrow_function.js

--- udemy_node/playground/2arrow_function.js
+++ udemy_node/playground/2arrow_function.js
@@ -1,3 +1,22 @@
-const square=function(x)
-{return x*x}
-console.log(square(3))
+// const square=function(x)
+// {return x*x}
+const square=(x)=>{
+return x*x}
+//OR
+// const square=(x)=> x*x  //for single statement
+console.log(square(4))
+
+//ES6
+const myEvent={
+    eventName:'Birthday party',
+    guestList:['Reena','Teena','Meena'],
+    printGuestList(){ //ES6 (shorthand instead of writing function)
+        console.log("Guest list for "+this.eventName)
+        this.guestList.forEach((guest) => { //arrow 2 function
+            console.log(guest+' is attending '+this.eventName)
+        })
+
+    }
+
+}
+myEvent.printGuestList()
