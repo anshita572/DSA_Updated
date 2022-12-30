f16c1c4b4aae027732d9f3bce94d36369c463ce3
anshita <anshita8150@gmail.com>
2022-12-31 00:05:51 +0530

    update min_swaps.cpp

--- RnBT_course/Recursion/Lecture 14/min_swaps.cpp
+++ RnBT_course/Recursion/Lecture 14/min_swaps.cpp
@@ -1,25 +1,32 @@
-//https://leetcode.com/problems/minimum-swaps-to-make-sequences-increasing/
+// https://leetcode.com/problems/minimum-swaps-to-make-sequences-increasing/
 
-//TLE (will be optimized with DP)
-#include<iostream>
+// TLE (will be optimized with DP)
+#include <iostream>
 #include <bits/stdc++.h>
 using namespace std;
-class Solution {
+class Solution
+{
 public:
-    int solve(vector<int>& nums1, vector<int>& nums2,int curr_index,int prev1,int prev2)
+    int solve(vector<int> &nums1, vector<int> &nums2, int curr_index, int prev1, int prev2)
     {
-        if(curr_index==nums1.size()) //base case
-        {return 0;} 
-        int no_swap=INT_MAX;
-        int swap=INT_MAX;
-        if(prev1<nums1[curr_index] && prev2<nums2[curr_index])
-        {no_swap=solve(nums1,nums2,curr_index+1,                  nums1[curr_index],nums2[curr_index]);}
-        if(prev2<nums1[curr_index] && prev1<nums2[curr_index]) 
-        {  swap=1+solve(nums1,nums2,curr_index+1,                  nums2[curr_index],nums1[curr_index]);}
-        return min(no_swap,swap);
-        
+        if (curr_index == nums1.size()) // base case
+        {
+            return 0;
+        }
+        int no_swap = INT_MAX;
+        int swap = INT_MAX;
+        if (prev1 < nums1[curr_index] && prev2 < nums2[curr_index])
+        {
+            no_swap = solve(nums1, nums2, curr_index + 1, nums1[curr_index], nums2[curr_index]);
+        }
+        if (prev2 < nums1[curr_index] && prev1 < nums2[curr_index])
+        {
+            swap = 1 + solve(nums1, nums2, curr_index + 1, nums2[curr_index], nums1[curr_index]);
+        }
+        return min(no_swap, swap);
     }
-    int minSwap(vector<int>& nums1, vector<int>& nums2) {
-        return solve(nums1,nums2,0,-1,-1);
+    int minSwap(vector<int> &nums1, vector<int> &nums2)
+    {
+        return solve(nums1, nums2, 0, -1, -1);
     }
 };
