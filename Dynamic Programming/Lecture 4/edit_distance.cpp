a1c7e1ea877164e9f0876fd957dfc622050c264d
anshita <anshita8150@gmail.com>
2022-12-30 19:47:35 +0530

    push edit_distance.cpp

---
+++ Dynamic Programming/Lecture 4/edit_distance.cpp
@@ -0,0 +1,76 @@
+//https://leetcode.com/problems/edit-distance/description/
+#include<iostream>
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    int solveTab(string s1, string s2)
+    {
+        vector<vector<int>> dp(s1.length()+1, vector<int>(s2.length()+1, 0));
+        // converting base cases
+        for (int j = 0; j < s2.length(); j++) // s1 has finished (s2>s1)
+        {
+            dp[s1.length()][j] = s2.length() - j;
+        }
+        for (int i = 0; i < s1.length(); i++)
+        {
+            dp[i][s2.length()] = s1.length() - i;
+        }
+        for (int i = s1.length() - 1; i >= 0; i--)
+        {
+            for (int j = s2.length() - 1; j >= 0; j--)
+            {
+                if (s1[i] == s2[j]) // match
+                {
+                    dp[i][j] = dp[i + 1][j + 1];
+                }    // no need of any operation ,check next char
+                else // not match
+                {
+                    int insertAns = 1 + dp[i][j + 1]; // 1+ means we are counting that operation
+                    int deleteAns = 1 + dp[i + 1][j];
+                    int replaceAns = 1 + dp[i + 1][j + 1];
+                    int ans = min(insertAns, min(deleteAns, replaceAns));
+                    dp[i][j] = ans;
+                }
+            }
+          
+        }
+          return dp[0][0];
+    }
+    int solve(string s1, string s2, int i, int j, vector<vector<int>> &dp)
+    {
+        // base case
+        if (i >= s1.length())
+        {
+            return s2.length() - j;
+        } // this number of operations will be added
+        if (j >= s2.length())
+        {
+            return s1.length() - i;
+        }
+        if (dp[i][j] != -1)
+        {
+            return dp[i][j];
+        }
+        if (s1[i] == s2[j]) // match
+        {
+            return dp[i][j] = 0 + solve(s1, s2, i + 1, j + 1, dp);
+        }    // no need of any operation ,check next char
+        else // not match
+        {
+            int insertAns = 1 + solve(s1, s2, i, j + 1, dp); // 1+ means we are counting that operation
+            int deleteAns = 1 + solve(s1, s2, i + 1, j, dp);
+            int replaceAns = 1 + solve(s1, s2, i + 1, j + 1, dp);
+            int ans = min(insertAns, min(deleteAns, replaceAns));
+            return dp[i][j] = ans;
+        }
+    }
+    int minDistance(string word1, string word2)
+    {
+        // vector<vector<int>> dp(word1.length(), vector<int>(word2.length(), -1));
+        // int ans = solve(word1, word2, 0, 0, dp);
+        // return ans;
+        return solveTab(word1,word2);
+    }
+};
