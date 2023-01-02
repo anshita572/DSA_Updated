7e3b357af75bdc1d539aefb8816f809b736ddd2c
anshita <anshita8150@gmail.com>
2023-01-03 00:13:56 +0530

    update detect_capital.cpp

--- POTD/Leetcode POTD/detect_captial.cpp
+++ POTD/Leetcode POTD/detect_captial.cpp
@@ -5,16 +5,16 @@ using namespace std;
 class Solution {
 public:
     bool detectCapitalUse(string word) {
-        int count=0;
+        int count=0;//count of capital letters
         for(int i=0;i<word.size();i++)
         {if(word[i]>='A' && word[i]<='Z')
         {count++;}
         }
-        if(count==0)
+        if(count==0)  //=> all small letters
         {return true;}
-       else if(count==word.size())
+       else if(count==word.size())  // => all capital letters
         {return true;}
-        else if(count == 1 && word[0]>='A' && word[0]<='Z')
+        else if(count == 1 && word[0]>='A' && word[0]<='Z')//=> 1st capital ,other letters small
         {return true;}
         else
         {return false;}
