fb6a95f138eabff16705de6046085518dcbfb22a
anshita <anshita8150@gmail.com>
2023-01-05 20:15:26 +0530

    update app.js

---
+++ udemy_node/weather_app/app.js
@@ -0,0 +1,5 @@
+const request=require('postman-request')
+const url="https://api.weatherstack.com/current?access_key=09187944b07f74bed43b43349282afa5&query=37.8267,-122.4233"
+request({url:url},(error,response)=>{
+console.log(response)
+})
