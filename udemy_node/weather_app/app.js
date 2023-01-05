e89dba61aaf272b71480189e57597fee9f47951e
anshita <anshita8150@gmail.com>
2023-01-05 17:13:45 +0530

    update app.js

--- udemy_node/weather_app/app.js
+++ udemy_node/weather_app/app.js
@@ -1,7 +1,11 @@
-console.log('Start')
-setTimeout (()=>{
-    console.log('Timer')},2000)
-console.log('Stop')
-//  setTimeout is 
-// asynchronous and non-blocking. The setTimeout call doesn’t block Node.js from running 
+console.log("Start");
+setTimeout(() => {
+  console.log("2 second timer");
+}, 2000);
+setTimeout(() => {
+    console.log("0 second timer");
+  }, 0);
+console.log("Stop");
+//  setTimeout is asynchronous and non-blocking.
+//  The setTimeout call doesn’t block Node.js from running
 // other code while it’s waiting for the 2 seconds to pass.
