c864abfadf2f9c0e7285668729f076f867c2fa86
anshita <anshita8150@gmail.com>
2023-01-07 19:10:16 +0530

    update app.js

--- udemy_node/weather_app/app.js
+++ udemy_node/weather_app/app.js
@@ -1,6 +1,7 @@
 const request = require("postman-request");
 const url =
-  "https://api.weatherstack.com/current?access_key=09187944b07f74bed43b43349282afa5&query=37.8267,-122.4233";
+  "http://api.weatherstack.com/current?access_key=09187944b07f74bed43b43349282afa5&query=37.8267,-122.4233";
 request({ url: url }, (error, response) => {
-  console.log(response);
+ const data = JSON.parse(response.body)
+ console.log(data.current) 
 });
