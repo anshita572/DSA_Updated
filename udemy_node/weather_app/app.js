61ac0da38a53050e1f3b36cc7a84a202f4b2e9cc
anshita <anshita8150@gmail.com>
2023-01-09 20:18:37 +0530

    update app.js

--- udemy_node/weather_app/app.js
+++ udemy_node/weather_app/app.js
@@ -1,7 +1,10 @@
 const request = require("postman-request");
 const url =
   "http://api.weatherstack.com/current?access_key=09187944b07f74bed43b43349282afa5&query=37.8267,-122.4233";
-request({ url: url }, (error, response) => {
- const data = JSON.parse(response.body)
- console.log(data.current) 
-});
+// request({ url: url }, (error, response) => {
+//  const data = JSON.parse(response.body)
+//  console.log(data.current) 
+// });
+request({ url: url,json:true }, (error, response) => {
+  console.log ("It is currently "+response.body.current.temperature+" degrees out.It feels like "+response.body.current.feelslike +" degrees out")
+ });
