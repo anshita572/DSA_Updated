dad501ffcd4d6bf68f01fc122625cff633326f16
anshita <anshita8150@gmail.com>
2023-01-10 23:47:47 +0530

    update app.js

--- udemy_node/weather_app/app.js
+++ udemy_node/weather_app/app.js
@@ -6,13 +6,20 @@ const url =
 //  console.log(data.current) 
 // });
 request({ url: url,json:true }, (error, response) => {
-  if(error)
+  if(error) //network error(internet not working)
   {console.log('Unable to connect to weather service !')}
-  else
+  else if(response.body.error) //some mistake in url
+  {console.log('Unable to find location')}
+  else //no error
   {console.log (response.body.current.weather_descriptions[0]+".It is currently "+response.body.current.temperature+" degrees out.It feels like "+response.body.current.feelslike +" degrees out")
 }
  });
-//  const url2="http://api.positionstack.com/v1/forward?access_key=a544a5edf0bb14d2bff03d0151c8775e&query=1600%20Pennsylvania%20Ave%20NW,%20Washington%20DC"
-//  request({url:url2,json:true},(error,response)=>{
-//   console.log("Latitude = "+response.body.data[0].latitude+" and Longitude = "+response.body.data[0].longitude)
-//  })
+ const url2="http://api.positionstack.com/v1/forward?access_key=a544a5edf0bb14d2bff03d0151c8775e&query=1600%20Pennsylvania%20Ave%20NW,%20Washington%20DC"
+ request({url:url2,json:true},(error,response)=>{
+  if(error)
+  {console.log('Unable to connect')}
+  else if(response.body.error)
+  {console.log('Unable to find geolocation')}
+  else
+  {console.log("Latitude = "+response.body.data[0].latitude+" and Longitude = "+response.body.data[0].longitude)}
+ })
