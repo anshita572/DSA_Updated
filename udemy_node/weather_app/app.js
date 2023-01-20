fc79e3ec94cb765b053c00fe1ccf7e0f86f6e6a3
anshita <anshita8150@gmail.com>
2023-01-20 20:01:09 +0530

    update app.js

--- udemy_node/weather_app/app.js
+++ udemy_node/weather_app/app.js
@@ -1,11 +1,11 @@
 const request = require("postman-request");
-const geocode=require('./utils/geocode')
-const forecast=require('./utils/forecast')
+const geocode = require("./utils/geocode");
+const forecast = require("./utils/forecast");
 // const url =
 //   "http://api.weatherstack.com/current?access_key=09187944b07f74bed43b43349282afa5&query=37.8267,-122.4233&units=f";
 // // request({ url: url }, (error, response) => {
 // //  const data = JSON.parse(response.body)
-// //  console.log(data.current) 
+// //  console.log(data.current)
 // // });
 
 // //Weather stack
@@ -28,15 +28,28 @@ const forecast=require('./utils/forecast')
 //   else
 //   {console.log("Latitude = "+response.body.data[0].latitude+" and Longitude = "+response.body.data[0].longitude)}
 //  })
-const apiKey="a544a5edf0bb14d2bff03d0151c8775e"
-const address="San Francisco"
- geocode('San Francisco',apiKey,(error,data)=>{
-  console.log('Error',error)
-  console.log('Data',data)
-  forecast(data.latitude,data.longitude,(error,data)=>{
-    console.log('Error',error)
-  console.log('Data',data)
- })
- })
- 
+// console.log(process.argv)
+const location=process.argv[2];
+if(!location)
+{console.log("Please enter a location")}
+else{
+  const apiKey = "a544a5edf0bb14d2bff03d0151c8775e";
+// const address = "San Francisco";
+geocode(location, apiKey, (error, data) => {
+  if (error) {
+    return console.log(error);
+  }
+  console.log("Error", error);
+  console.log("Data", data);
+  forecast(data.latitude, data.longitude, (error, forecastData) => {
+    if (error) {
+      return console.log(error);
+    }
+    console.log(data.label);
+    console.log(forecastData);
+  });
+});
+
 //  encodeURIComponent(address)
+}
+
