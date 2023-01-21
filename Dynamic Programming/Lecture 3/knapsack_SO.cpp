3b0068a8e22574c026de527eb42aff5d2f75e9c8
anshita <anshita8150@gmail.com>
2023-01-21 16:57:44 +0530

    push space optimised 0/1 knapsack

---
+++ Dynamic Programming/Lecture 3/knapsack_SO.cpp
@@ -0,0 +1,86 @@
+//https://practice.geeksforgeeks.org/problems/0-1-knapsack-problem0945/1
+
+//https://takeuforward.org/data-structure/0-1-knapsack-dp-19/
+#include<iostream>
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+    public:
+    int solveSO2(int cap, int wt[], int val[], int n)
+    {vector<int>curr(cap+1,0);
+       for(int i=wt[0]; i<=cap; i++){
+        curr[i] = val[0];
+    }
+    for(int i=1;i<n;i++)
+        { for(int W=cap;W>=0;W--)
+        {int inc=0;
+        if(W>=wt[i])
+       {  inc=val[i]+curr[W-wt[i]];}
+        int exc=curr[W];
+         curr[W]= max(inc,exc);
+        }
+        }
+        return curr[cap];
+    }
+    int solveSO(int cap, int wt[], int val[], int n)
+    {vector<int>curr(cap+1,0);
+     vector<int>prev(cap+1,0);
+       for(int i=wt[0]; i<=cap; i++){
+        prev[i] = val[0];
+    }
+    for(int i=1;i<n;i++)
+        { for(int W=0;W<=cap;W++)
+        {int inc=0;
+        if(W>=wt[i])
+       {  inc=val[i]+prev[W-wt[i]];}
+        int exc=prev[W];
+         curr[W]= max(inc,exc);
+        }
+        prev=curr;
+        }
+        return prev[cap];
+    }
+    int solveTab(int cap, int wt[], int val[], int n){
+        vector<vector<int>>dp(n+1,vector<int>(cap+1,0));
+           for(int i=wt[0]; i<=cap; i++){//Analyse base case
+        dp[0][i] = val[0];//initialsie 0th row of dp vector with val[0]
+    }
+      for(int i=1;i<n;i++)
+        { for(int W=0;W<=cap;W++)
+            {int inc=0;
+        if(W>=wt[i])
+       {  inc=val[i]+dp[i-1][W-wt[i]];}
+        int exc=dp[i-1][W];
+         dp[i][W]= max(inc,exc);}
+        }
+        return dp[n-1][cap];
+    }
+    int solve(int W, int wt[], int val[], int n,int i,vector<vector<int>>&dp)
+    {
+        if(i==0)
+        {if(W>=wt[0]) //I am at last index and my knapsack still has capacity
+        //to accomodate last item(wt[0]) then just take it
+            {return val[0];}
+        else
+        {return 0;}
+        }
+        if(dp[i][W]!=-1)
+        {return dp[i][W];}
+        int inc=0;
+        if(W>=wt[i])
+       {  inc=val[i]+solve(W-wt[i],wt,val,n,i-1,dp);}
+        int exc=solve(W,wt,val,n,i-1,dp);
+        return dp[i][W]= max(inc,exc);
+        
+    }
+    //Function to return max value that can be put in knapsack of capacity W.
+    int knapSack(int W, int wt[], int val[], int n) 
+    { 
+    //     vector<vector<int>>dp(n,vector<int>(W+1,-1));
+    //   return solve(W,wt,val,n,n-1,dp);
+    // return solveTab(W,wt,val,n);
+    // return solveSO(W,wt,val,n);
+    return solveSO2(W,wt,val,n);
+    }
+};
