a2b96378569c7b3007f4ac93b53bfc8f6a760790
anshita <anshita8150@gmail.com>
2023-01-26 13:37:55 +0530

    install hbs

--- udemy_node/web_server/src/app.js
+++ udemy_node/web_server/src/app.js
@@ -1,14 +1,25 @@
 const path=require('path')
 const express=require('express')
 const app=express()
+
+app.set('view engine','hbs')  //to set handlebars after installing it for dynamic sites
 // console.log(__dirname) //displays path till current directory
 // console.log(__filename) //displays path till current file
 // console.log(path.join(__dirname,'../public'))
-const directoryPath=path.join(__dirname,'../public')
-app.use(express.static(directoryPath))
+
+
+
+// app.get('',(req,res)=>{
+//     res.send("<h1>hi</h1>")
+// })
 app.get('',(req,res)=>{
-    res.send("<h1>hi</h1>")
+    res.render('index',{
+        title : 'using hbs from app.js',
+        myName: 'Akshu'
+    }) //render is used for views handlebars
 })
+const directoryPath=path.join(__dirname,'../public')
+app.use(express.static(directoryPath)) //for index.html in public folder
 // app.get('/about',(req,res)=>{
 //     res.send([{
 //         myName :'Anshita',
