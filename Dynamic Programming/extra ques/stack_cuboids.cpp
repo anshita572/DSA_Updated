3d8391166bb82863cc0a4bf4e563506512144345
anshita <anshita8150@gmail.com>
2023-02-16 20:05:32 +0530

    push stack_cuboids.cpp

--- Dynamic Programming/extra ques/stack_cuboids.cpp
+++ Dynamic Programming/extra ques/stack_cuboids.cpp
@@ -7,15 +7,55 @@ class Solution
 public:
     bool isSafe(vector<vector<int>> &cuboids, int prev, int curr)
     {
-        if (cuboids[curr][0] <= cuboids[prev][0] && cuboids[curr][1] <= cuboids[prev][1] && cuboids[curr][2] <= cuboids[prev][2])
+        if (cuboids[curr][0] >= cuboids[prev][0] && cuboids[curr][1] >= cuboids[prev][1] && cuboids[curr][2] >= cuboids[prev][2])
         {
             return true;
         }
         return false;
     }
+    int solveSO(vector<vector<int>> &cuboids)
+    {
+        int n = cuboids.size();
+        vector<int> currRow(n + 1, 0);
+        vector<int> nextRow(n + 1, 0);
+        for (int curr = n - 1; curr >= 0; curr--)
+        {
+            for (int prev = n - 1; prev >= -1; prev--)
+            {
+                int inc = 0;
+                if (prev == -1 || isSafe(cuboids, prev, curr))
+                {
+                    inc = cuboids[curr][2] + nextRow[curr + 1];
+                }
+                int exc = nextRow[prev + 1];
+                currRow[prev + 1] = max(inc, exc);
+            }
+            nextRow = currRow;
+        }
+        return currRow[0];
+    }
+    int solveTab(vector<vector<int>> &cuboids)
+    {
+        int n = cuboids.size();
+        vector<vector<int>> dp(n + 1, (vector<int>(n + 1, 0)));
+        for (int curr = n - 1; curr >= 0; curr--)
+        {
+            for (int prev = n - 1; prev >= -1; prev--)
+            {
+                int inc = 0;
+                if (prev == -1 || isSafe(cuboids, prev, curr))
+                {
+                    inc = cuboids[curr][2] + dp[curr + 1][curr + 1];
+                }
+                int exc = dp[prev + 1][curr + 1];
+                dp[prev + 1][curr] = max(inc, exc);
+            }
+        }
+        return dp[0][0];
+    }
     int solve(vector<vector<int>> &cuboids, int prev, int curr, vector<vector<int>> &dp)
     {
-        if (curr < 0)
+        if (curr == cuboids.size())
         {
             return 0;
         }
@@ -26,9 +66,9 @@ public:
         int inc = 0;
         if (prev == -1 || isSafe(cuboids, prev, curr))
         {
-            inc = cuboids[curr][2] + solve(cuboids, curr, curr - 1, dp);
+            inc = cuboids[curr][2] + solve(cuboids, curr, curr + 1, dp);
         }
-        int exc = solve(cuboids, prev, curr - 1, dp);
+        int exc = solve(cuboids, prev, curr + 1, dp);
         return dp[prev + 1][curr] = max(inc, exc);
     }
     int maxHeight(vector<vector<int>> &cuboids)
@@ -38,8 +78,10 @@ public:
         {
             sort(cuboids[i].begin(), cuboids[i].end());
         }
-        sort(cuboids.begin(), cuboids.end()); // sort all according to width
-        vector<vector<int>> dp(n + 1, (vector<int>(n, -1)));
-        return solve(cuboids, -1, n - 1, dp);
+        sort(cuboids.begin(), cuboids.end()); // sort all acoording to width
+        //     vector<vector<int>>dp(n+1,(vector<int>(n,-1)));
+        // return solve(cuboids,-1,0,dp);
+        // return solveTab(cuboids);
+        return solveSO(cuboids);
     }
 };
