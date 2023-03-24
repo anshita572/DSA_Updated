78d54cafd2f0849d09a6d51b36a6df2261ece5e4
anshita <anshita8150@gmail.com>
2023-03-24 20:29:28 +0530

    push react-app

---
+++ react-app/src/Message.tsx
@@ -0,0 +1,6 @@
+//Pascal casing (1st letter of function name capital (here M))
+function Message()
+{//JSX : Javascript XML
+    const name='Anshita'
+    return <h1>Hello {name}</h1>;}
+export default Message;
