12341e235921603bb31e599bfc45d840b7324dd5
anshita <anshita8150@gmail.com>
2023-03-28 16:06:21 +0530

    push update

--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -1,16 +1,28 @@
 import { Fragment } from "react";
+import { MouseEvent } from "react";
 function ListGroup(){
+  let items=["Patiala","Ludhiana","Delhi","Mumbai"]
+  //event handler
+  const handleClick=(event : MouseEvent)=>console.log(event);
+  // items=[];
     return (
         <>
     <h1>List</h1>
+    {/* {items.length===0 ? <p>No item found</p> : null} */}
+    {items.length===0 && <p>No item found</p>}  
+    {/* if no item then say no item found */}
     <ul className="list-group">
-    <li className="list-group-item">An item</li>
-    <li className="list-group-item">A second item</li>
-    <li className="list-group-item">A third item</li>
-    <li className="list-group-item">A fourth item</li>
-    <li className="list-group-item">And a fifth one</li>
+    {items.map((i,index)=>(
+      <li className="list-group-item"
+       key={i}
+      //  onClick={()=>console.log(i,index)}
+      onClick={handleClick}
+       >
+      
+        {i}</li>
+    ))}
   </ul>
   </>
-    );
+    ); //items.map((i) => loop
 }
 export default ListGroup;
