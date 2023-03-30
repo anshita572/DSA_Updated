87cfb13391b9e9bc6685ddb9a034354e015e4a00
anshita <anshita8150@gmail.com>
2023-03-31 00:59:43 +0530

    update sidebar

--- react-portfolio/src/components/Sidebar/index.js
+++ react-portfolio/src/components/Sidebar/index.js
@@ -1,14 +1,27 @@
-import { Link } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 import './index.scss'
 import LogoS from '../../assets/images/logo-s.png'
 import LogoSubtitle from '../../assets/images/logo_sub.png'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import{faEnvelope, faHome,faUser}from '@fortawesome/free-solid-svg-icons'
 const Sidebar=()=>(
     <div className='nav-bar'>
         <Link className='logo' to = '/'> 
   {/* '/' represents home page */}
   <img src={LogoS} alt="logo"/>
-  <img src={LogoSubtitle} alt="slobodan"/>
+  <img className="sub-logo "src={LogoSubtitle} alt="slobodan"/>
         </Link>
+        <nav>
+            <NavLink exact="true" activeclassname="active" to="/">
+                <FontAwesomeIcon icon={faHome} color="#4d4d4e"/>
+            </NavLink>
+            <NavLink exact="true" activeclassname="active" to="/">
+                <FontAwesomeIcon icon={faUser} color="#4d4d4e"/>
+            </NavLink>
+            <NavLink exact="true" activeclassname="active" to="/">
+                <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e"/>
+            </NavLink>
+        </nav>
     </div>
 )
 
