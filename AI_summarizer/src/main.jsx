5e72eca22a3ad80aea6b85046b399e99969b5295
anshita <anshita8150@gmail.com>
2023-05-31 20:14:59 +0530

    push AI_summarizer

--- AI_summarizer/src/main.jsx
+++ AI_summarizer/src/main.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { Provider } from 'react-redux'
+import App from './App'
+import {store} from './services/store'
+// import {Theme} from './dark_mode'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <Provider store={store}>
     <App />
+    {/* <Theme/> */}
+    </Provider>
   </React.StrictMode>,
 )
